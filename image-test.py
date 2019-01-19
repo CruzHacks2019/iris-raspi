36ddd413d3c024727bbd995e5a9d40dff1632ecb
Andrew Zhu <me@andrew-zhu.com>
2019-01-18 23:35:16 -0800

    takes an image and send it to the cloudtm

--- image-test.py
+++ image-test.py
@@ -1,7 +1,11 @@
+import sys
 from base64 import b64encode
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
+import requests
+
+API_BASE = sys.argv[-1]
 
 # Explicitly open a new file called my_image.jpg
 io = BytesIO()
@@ -12,3 +16,4 @@ camera.capture(io, 'jpeg')
 b64 = b64encode(io.getvalue())
 
 print(b64.decode())
+requests.post(f"{API_BASE}/detect-face", data=b64.decode())
