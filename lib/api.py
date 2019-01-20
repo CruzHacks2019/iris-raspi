ac10de9f0e87b1d1dae027eaddd8c3a8ec517150
Tejas Shah <tejashah88@gmail.com>
2019-01-20 03:21:31 +0000

    support for multiple models + other stuff in the works

--- lib/api.py
+++ lib/api.py
@@ -2,11 +2,20 @@ import sys
 
 import requests
 
-API_BASE = "http://5dd7b57f.ngrok.io" # sys.argv[-1]
+API_BASE = "http://1f757ced.ngrok.io" # sys.argv[-1]
 
 def post_face(face):
-    """Posts face (a base-64 encoded bytes object) and returns... something."""
+    """Posts face (a base-64 encoded bytes object) and returns...something."""
     response = requests.post(f"{API_BASE}/detect-face", data=face.decode())
+    try:
+        return response.json()
+    except Exception as ex:
+        print(response.text)
+        return None
+
+def get_reminders():
+    """Retrieves all reminders for use with proactive notifications"""
+    response = requests.get(f"{API_BASE}/reminders")
     try:
         return response.json()
     except Exception as ex:
--- lib/client.py
+++ lib/client.py
@@ -1,13 +1,30 @@
 from snowboy import snowboydecoder
 import sys
 import signal
+import time
 
 from funcs import capture_image_usb, say
-from api import post_face
+from api import post_face, get_reminders
 
 interrupted = False
 
-def on_hotword_trigger():
+recent_face_id = False
+recent_face_id_time = -1
+max_expire_time = 60 * 1000
+
+recent_user = None
+
+reminders = get_reminders()
+if reminders is None:
+    print("Warning: couldn't obtain reminders!")
+
+epoch = lambda: int(time.time() * 1000)
+
+def on_req_face_identify():
+    global recent_face_id, recent_face_id_time, max_expire_time
+    print("on_req_face_identify")
+    # return
+
     print("> Capturing image from USB camera...", end="")
     face64 = capture_image_usb()
     print("done!")
@@ -19,32 +36,83 @@ def on_hotword_trigger():
     print("RESPONSE")
     print(response)
 
-    if response and response.msg:
-        say(response.msg)
+    if response is None:
+        say("An unknown error has occurred!")
+    elif 'msg' in response:
+        say(response['msg'])
+        recent_face_id = True
+        recent_face_id_time = epoch()
+    elif 'error' in response:
+        say(response['error'])
+    else:
+        say("An unknown error has occurred!")
+
+# TODO: implement requesting more info
+def on_req_more_info():
+    global recent_face_id, recent_face_id_time, max_expire_time
+    print("on_req_more_info")
+
+    # check if previous command was called or if just enough time has passed for the context to be relevent
+    if not recent_face_id or epoch() - recent_face_id_time > max_expire_time:
+        print("  false alarm")
+        return
+
+    recent_face_id = False
+    recent_face_id_time = -1
+
+    pass
 
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
+# TODO: implement reminder check and alerts
+# use interupt_callback to check time continuously
 def interrupt_callback():
     global interrupted
+    
     return interrupted
 
 if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python client.py your.model")
+    print("Error: need to specify the patient's name")
+    print("Usage: python lib/client.py john")
     sys.exit(-1)
 
-model = sys.argv[1]
+person = sys.argv[1]
+
+face_id_models = [
+  f"who_is_that-{person}",
+  f"who_are_you-{person}",
+  f"dont_remember_you-{person}"
+]
+
+face_id_callbacks = [on_req_face_identify] * len(face_id_models)
+
+more_info_models = [
+  f"i_dont_understand-{person}",
+  f"still_dont_remember-{person}"
+]
+
+more_info_callbacks = [on_req_more_info] * len(more_info_models)
+
+# assemble all to-be-loaded models for processing
+combined_models = face_id_models + more_info_models
+final_models = ["hotword-models/" + model + ".pmdl" for model in combined_models]
+
+# merge all callback paths
+combined_callbacks = face_id_callbacks + more_info_callbacks
+
+# set sensitivities for all models
+sensitivity_list = [0.5] * len(final_models)
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, audio_gain=1, sensitivity=0.5)
+detector = snowboydecoder.HotwordDetector(final_models, audio_gain=1, sensitivity=sensitivity_list)
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
-detector.start(detected_callback=on_hotword_trigger,
+detector.start(detected_callback=combined_callbacks,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
